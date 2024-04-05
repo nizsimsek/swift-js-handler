75f2b77d80d9a874396fbde1fcd156c255657b02
Nizamettin Şimşek <nizsimsek@icloud.com>
2024-04-05 16:52:06 +0300

    ASD

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,6 +1,4 @@
-import About from "@/pages/about";
 import Login from "@/pages/auth/login";
-import Home from "@/pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "@/pages/auth/register";
 import Room from "@/pages/room";
@@ -11,8 +9,6 @@ const LiveRouter = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/room" element={<Room />} />
         <Route path="/room/:roomId" element={<Room />} />
       </Routes>
